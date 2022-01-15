a564fa827bc870761fdb94236db2a4a01b17544b
dan <danieljamieprice@gmail.com>
2022-01-15 19:44:01 +0000

    Complete book object constructor and methods call global functions 'addBookToLibrary' and 'assignId' in constructor add prototype method 'setReadState'

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,35 @@
-let myLibrary = [];
-let idCount = 0;
-
+//constructor
 function Book(author, title, numberOfPages) {
     this.author = author;
     this.title = title;
     this.numberOfPages = numberOfPages;
     this.isRead = false;
-    this.id = assignId(this);
+    this.id = null;
+    addBookToLibrary(this);
+    assignId(this);
+}
+//prototype methods
+Book.prototype.setReadState = function() {
+    this.isRead = this.isRead === false ? true : false;
+}
+Book.prototype.getInfo = function() {
+    return "Author: " + this.author + "\n" + "Title: " + this.title +
+    "\n" + "Number of pages: " + this.numberOfPages;  
 }
 
-function addBookToLibrary() {
-  // do stuff here
+//global functions
+function addBookToLibrary(book) {
+    myLibrary.push(book);
 }
-
 function assignId(book) {
-    book.id = idCount;
-    increaseIdCount();
+    book.id = myLibrary.indexOf(book);
 }
+function removeBookFromLibrary(book) {}
 
-function increaseIdCount() {
-    idCount += 1;
-}
-
-function isRead(book) {
-    return book.isRead;
-}
-
-function setReadState(book) {
-    book.isRead = isRead(book) === false ? true : false;
-}
+//declarations
+let myLibrary = [];
+const newBook1 = new Book("Mr author", "the title", "200");
+const newBook2 = new Book("Mr x", "title x", "300");
+const newBook3 = new Book("Mr y", "title y", "400");
+const newBook4 = new Book("Mr z", "title z", "500");
+console.log(myLibrary);
