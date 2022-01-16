e6ef8154623f04b95a785afc604c5e14f95594f2
dan <danieljamieprice@gmail.com>
2022-01-16 18:39:28 +0000

    add modal popup for add book

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 //constructor
 function Book(author, title, numberOfPages) {
-    this.author = author;
     this.title = title;
+    this.author = author;
     this.numberOfPages = numberOfPages;
     this.isRead = false;
     this.id = null;
@@ -20,12 +20,18 @@ function addBookToLibrary(book) {
 function assignId(book) {
     book.id = myLibrary.indexOf(book);
 }
-function removeBookFromLibrary(book) {}
 
 //declarations
+const addBookButton = document.getElementById("add-book-btn");
+const modal = document.getElementById("modal");
 let myLibrary = [];
 const newBook1 = new Book("Mr author", "the title", "200");
 const newBook2 = new Book("Mr x", "title x", "300");
 const newBook3 = new Book("Mr y", "title y", "400");
 const newBook4 = new Book("Mr z", "title z", "500");
 console.log(myLibrary);
+
+//event listeners
+addBookButton.addEventListener("click", (e) => {
+    modal.style.display = modal.style.display === "none" ? "block" : "none";
+})
